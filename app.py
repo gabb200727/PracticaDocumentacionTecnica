b4791bed6c869f3b2720da239d7bd9e2698e289d
Gabriel <mattramrz96@gmail.com>
2026-05-21 20:25:43 -0500

    Programa que suma dos numeros

---
+++ app.py
@@ -0,0 +1,11 @@
+# Programa sencillo para sumar dos números
+
+# Pedir los números al usuario
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Realizar la suma
+suma = num1 + num2
+
+# Mostrar el resultado
+print("La suma es:", suma)
